05f908b5e91d60f3355ddceb5a7a2cfe8a385f1b
Berlin <evan1234.ek@gmail.com>
2023-11-03 17:29:07 +0600

    Refactor game query state in App component and update GameGrid component to use it

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,16 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames, { Platform } from '../hooks/useGames';
+import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
-import { Genre } from '../hooks/useGenres';
+import { GameQuery } from '../App';
 
 interface Props {
-  seletedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({ seletedGenre, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = useGames(seletedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
 
   const skeletons = Array.from({ length: 20 }, (_, i) => i);
 

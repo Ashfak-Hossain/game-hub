ae9f48be82a8e827632caab0bee8ffc4b98a347d
Berlin <evan1234.ek@gmail.com>
2023-11-03 11:04:06 +0600

    Update styles and hooks for game and genre components

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,13 +5,11 @@ import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, loading } = useGames();
 
-  const skeletons = Array.from({ length: 10 }, (_, i) => i);
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) {
-    return <Text>Error: {error}</Text>;
-  }
+  if (error) return <Text>Error: {error}</Text>;
 
   return (
     <>
@@ -20,13 +18,13 @@ const GameGrid = () => {
         padding="10px"
         spacing={10}
       >
-        {isLoading &&
+        {loading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {games?.map((game) => (
+        {data?.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
